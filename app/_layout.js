213f6de7fdc3f2610fea89acdd39ee6e02e32b42
junior <hamdidajr@email.com>
2026-04-11 22:15:06 +0100

    try one signal

--- app/_layout.js
+++ app/_layout.js
@@ -2,6 +2,17 @@ import { Stack } from "expo-router";
 import { useEffect } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import i18n from "../i18n";
+import {OneSignal} from "react-native-onesignal"
+import {isDevice} from "expo-device"
+
+if(isDevice){
+  OneSignal.initialize("7775cac3-1d16-4592-a1b1-52f11b733d87")
+  OneSignal.Notifications.requestPermission(true)
+  OneSignal.Notifications.addEventListener("foregroundWillDisplay",(event)=>{
+    event.preventDefault()
+    event.getNotification().display()
+  })
+}
 
 export default function Layout() {
   useEffect(() => {
