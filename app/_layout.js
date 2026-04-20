f940e0d7f9d306495a34b23b57d02e55c4ce5fa3
junior <hamdidajr@email.com>
2026-04-20 15:51:53 +0100

    update th not s

--- app/_layout.js
+++ app/_layout.js
@@ -6,33 +6,52 @@ import {OneSignal} from "react-native-onesignal"
 import * as Device from "expo-device"
 
 
+
+  const api_url = "https://ox-mvpp.onrender.com"
+
+
 export default function Layout() {
-  RootLayout(){
     useEffect(()=>{
       const initOneSignal = async()=>{
+      const savedLang = await AsyncStorage.getItem("language");
+      if (savedLang) {
+        i18n.locale = savedLang;
+      }
         if(!Device.isDevice){
           console.log("not a real device");
           return;
-        }
+        }//commit
           OneSignal.initialize("7775cac3-1d16-4592-a1b1-52f11b733d87");
           await OneSignal.Notifications.requestPermission(true);
           const id = await OneSignal.User.pushSubscription.getIdAsync();
           console.log("onesignal id : ",id);
+          //get user info
+          const userId = await AsyncStorage.getItem("userId")
+          if(id && userId){
+            try {
+              await fetch(`${api_url}/api/save-onesignal-id`,{
+                method:"POST",
+                header:{
+                  "Content-Type":"application/json",
+                },
+                body:JSON.stringify({
+                  userId,
+                  id,
+                }),
+              });
+              console.log("onesignal id saved to backend")
+            }catch(err){
+              console.log("error ",err)
+            }
+          }
       };
       initOneSignal();
     },[])
 
   useEffect(() => {
-    const loadLang = async () => {
-      const savedLang = await AsyncStorage.getItem("language");
-      if (savedLang) {
-        i18n.locale = savedLang;
-      }
-    };
-    loadLang();
+  
   }, []);
 
 
   return <Stack screenOptions={{ headerShown: false }} />; 
- };
 }
