bafe08ea73203eb4b0b37509570600a299b78ed2
junior <hamdidajr@email.com>
2026-04-22 12:16:15 +0100

    add the first step of authentication

--- app/_layout.js
+++ app/_layout.js
@@ -2,8 +2,8 @@ import { Stack } from "expo-router";
 import { useEffect } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import i18n from "../i18n";
-import {OneSignal} from "react-native-onesignal"
-import * as Device from "expo-device"
+import { router } from "expo-router";
+import { useState } from "react";
 
 
 
@@ -11,47 +11,23 @@ import * as Device from "expo-device"
 
 
 export default function Layout() {
+  const [isLoggedIn , setIsLoggedIn] = useState(null)
     useEffect(()=>{
-      const initOneSignal = async()=>{
+      const init = async()=>{
+        const userId = await AsyncStorage.getItem("userId")
+        setIsLoggedIn(!!userId) 
       const savedLang = await AsyncStorage.getItem("language");
       if (savedLang) {
         i18n.locale = savedLang;
-      }
-        if(!Device.isDevice){
-          console.log("not a real device");
-          return;
-        }//commit
-          OneSignal.initialize("7775cac3-1d16-4592-a1b1-52f11b733d87");
-          await OneSignal.Notifications.requestPermission(true);
-          const id = await OneSignal.User.pushSubscription.getIdAsync();
-          console.log("onesignal id : ",id);
-          //get user info
-          const userId = await AsyncStorage.getItem("userId")
-          if(id && userId){
-            try {
-              await fetch(`${api_url}/api/save-onesignal-id`,{
-                method:"POST",
-                header:{
-                  "Content-Type":"application/json",
-                },
-                body:JSON.stringify({
-                  userId,
-                  id,
-                }),
-              });
-              console.log("onesignal id saved to backend")
-            }catch(err){
-              console.log("error ",err)
-            }
-          }
+      }          
       };
-      initOneSignal();
+      init();
     },[])
+    if(isLoggedIn === null ) return null 
+    if(!isLoggedIn){
+      return router.replace("/signup");
 
-  useEffect(() => {
-  
-  }, []);
-
+    }
 
   return <Stack screenOptions={{ headerShown: false }} />; 
 }
